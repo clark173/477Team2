621140f8e392ff9b7139cb8b8ab396910b0ca961
clark173 <clark173@purdue.edu>
2015-09-24 12:09:56 -0400

    Now using UART to send out the data package to a connected device.

--- scan_main.py
+++ scan_main.py
@@ -1,27 +1,42 @@
 import json
+import serial
+import sys
+import time
 import urllib2
 
+
+BAUD = 115200
+PORT = '/dev/ttyAMA0'
 
 UPC_DATABASE_URL = 'http://api.upcdatabase.org/json/'
 UPC_LOOKUP_API_KEY = '85043ef8601462a39e97cee08c9baa5b/'
 
 
-def string_to_hex(name):
-    hex_array = []
-    for letter in name:
-        hex_val = hex(ord(letter))
-        hex_array.append(hex_val)
-    return hex_array
+# Data Structure:
+# <UPC_CODE><NAME><SERVINGS><DATE_IN><IN/OUT>
+def create_package(barcode, name, servings, in_out):
+    return '<%s><%s><%s><%s><%s>\r\n' % (barcode, name, servings,
+                                     time.strftime("%m/%d/%Y"), in_out)
+
+def send_uart_data(package):
+    port = serial.Serial(PORT, BAUD)
+    port.write(package)
 
 def get_item_name(upc_dictionary):
-    item_name = upc_dictionary['itemname']
-    description = upc_dictionary['description']
-    if item_name == "":
-        return description.title()
-    else:
-        return item_name.title()
+    try:
+        item_name = upc_dictionary['itemname']
+        description = upc_dictionary['description']
+        if item_name == "":
+            return description.title()
+        else:
+            return item_name.title()
+    except KeyError:
+        return None
 
 def lookup_barcode(barcode):
+    # API doesn't accept the first int in the barcode
+    if (len(barcode) % 2) == 1:
+        barcode = barcode[1:]
     api_request = urllib2.Request('%s%s%s' % (UPC_DATABASE_URL,
                                               UPC_LOOKUP_API_KEY,
                                               barcode))
@@ -30,8 +45,15 @@ def lookup_barcode(barcode):
 
 if __name__ == "__main__":
     while True:
-        barcode = raw_input('UPC-A Barcode: ')
-        # API doesn't accept first int in UPC-A barcode
-        upc_dictionary = lookup_barcode(barcode[1:])
+        try:
+            barcode = raw_input('UPC-A Barcode: ')
+        except KeyboardInterrupt:
+            print '\nKeyboard Interrupt. Exiting Script'
+            sys.exit(1)
+        upc_dictionary = lookup_barcode(barcode)
         name = get_item_name(upc_dictionary)
-        hex_array = string_to_hex(name)
+        if name is None:
+            print 'Invalid barcode'
+            continue
+        package = create_package(barcode, name, 1, 'IN')
+        send_uart_data(package)
